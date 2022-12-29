6e16ae28ca9c72dc820e546f7fa801891998af34
Sibasis Badatya <98338367+Sibasisbadatya@users.noreply.github.com>
2022-12-29 09:36:34 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,8 @@
 
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://localhost:27017/sibasis",{
+const MONGO_URI="mongodb+srv://sibasis:iNEyBvsvWXZ0BZwI@cluster0.eptzsb1.mongodb.net/"
+mongoose.connect(MONGO_URI,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
@@ -10,4 +11,4 @@ mongoose.connect("mongodb://localhost:27017/sibasis",{
 }).catch((e)=>{
     console.log("not connected");
 })
-
+// mongodb://localhost:27017/sibasis
